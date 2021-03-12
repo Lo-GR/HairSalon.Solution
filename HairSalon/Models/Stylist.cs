9afc5f583c5ff8e1e3ea94a2a669e82f20cfeab0
Lo-GR <diamondintheroth@gmail.com>
2021-03-12 11:09:47 -0800

    add Index view for stylist

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -8,7 +8,7 @@ namespace HairSalon.Models
     {
       this.Clients = new HashSet<Client>();
     }
-    public int ClientId {get; set;}
+    public int StylistId {get; set;}
     public string Name {get; set;}
     public string Expertise {get; set;}
     public virtual ICollection<Client> Clients {get; set;}
