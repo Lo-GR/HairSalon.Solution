34c2917ebafb631a50f0a3741c21622811909faf
Lo-GR <diamondintheroth@gmail.com>
2021-03-12 10:57:06 -0800

    create stylists controller

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+  }
+}
